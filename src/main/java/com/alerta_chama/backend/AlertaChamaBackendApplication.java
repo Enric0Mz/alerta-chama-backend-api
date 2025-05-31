1bdc230965e969750bfc1e62fd7d94f19555976a
Enrico Marquez <enricovmarquezz@gmail.com>
2025-05-31 16:58:01 -0300

    build: initialize project setup with `spring boot`

---
+++ src/main/java/com/alerta_chama/backend/AlertaChamaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alerta_chama.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaChamaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaChamaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alerta_chama/backend/AlertaChamaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alerta_chama.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaChamaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
